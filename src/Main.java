a0efca7b387ec6a14a267ed10bc0548005fec23c
Solo <ksolo15mtetwa@outlook.com>
2023-12-21 02:51:01 +0100

    Created a new main method with Dog and Cat Objects

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,23 @@
 public class Main {
+    public static void main(String[] args) {
+        HomeAnimal<Dog> dogHome = new HomeAnimal<>(new Dog());
+        dogHome.getAnimal().run();
+        dogHome.getAnimal().speak();
+        dogHome.showType();
+        dogHome.sleepOnTheCouch();
+
+        HomeAnimal<Cat> catHome = new HomeAnimal<>(new Cat());
+        catHome.getAnimal().run();
+        catHome.getAnimal().speak();
+        catHome.showType();
+        catHome.sleepOnTheCouch();
+
+        //HomeAnimal<Shark> SharkHome = new HomeAnimal<>(new Shark());
+        //SharkHome.getAnimal().run();
+        //SharkHome.getAnimal().speak();
+        //SharkHome.showType();
+        //SharkHome.sleepOnTheCouch();
+    }
 
 }
 
@@ -13,6 +32,33 @@ interface Speak {
     public void speak();
 }
 
+class Cat extends Animal implements Run , Speak{
+    @Override
+    public void run() {
+        System.out.println("the cat is running.");
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("the cat is meowing.");
+    }
+}
+
+class Dog extends Animal implements Run , Speak{
+    @Override
+    public void run() {
+        System.out.println("the dog is running.");
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("the dog is barking .");
+    }
+}
+
+class Shark extends Animal{
+
+}
 class HomeAnimal<T extends Animal & Run & Speak> {
     private T animal;
 
