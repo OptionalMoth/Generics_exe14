efe4a8e37644f05ac00aa9ae6656660be8ad9e51
Solo <ksolo15mtetwa@outlook.com>
2023-12-21 02:43:02 +0100

    Created a new class HomeAnimal, and it implements  the interfaces Run and Speak

--- src/Main.java
+++ src/Main.java
@@ -12,3 +12,23 @@ interface Run {
 interface Speak {
     public void speak();
 }
+
+class HomeAnimal<T extends Animal & Run & Speak> {
+    private T animal;
+
+    public HomeAnimal(T animal) {
+        this.animal = animal;
+    }
+
+    public T getAnimal() {
+        return animal;
+    }
+
+    public void showType() {
+        System.out.println("Type of animal: " + animal.getClass().getSimpleName());
+    }
+
+    public void sleepOnTheCouch() {
+        System.out.println("The animal is sleeping on the couch.");
+    }
+}
